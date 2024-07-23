dea4df27a1478d4bf2aed1a302bc79422673d72f
Tomás Maritano <tomymaritano@gmail.com>
2024-07-22 23:18:36 -0300

    updates

--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -4,8 +4,6 @@ import {
   Button
 } from "@chakra-ui/react";
 import Github from "../components/Github";
-import VideoComponent from "../components/VideoComponent";
-import VideoComponent2 from "../components/VideoComponent2";
 
 
 const ProjectsPage = () => {
@@ -17,13 +15,13 @@ const ProjectsPage = () => {
           <Box bg={'customGray'} color={'customBackground'} p={4} borderRadius={'lg'}>
             <Text fontSize={'2xl'} as={'b'}>Unicoin Dashboard</Text>
             <Text fontSize={'lg'} pb={10}>Investors Platform</Text>
-            <VideoComponent />
+    
             <Button>View Project</Button>
           </Box>
           <Box bg={'customGray'} color={'customBackground'} p={4} borderRadius={'lg'}>
             <Text fontSize={'2xl'} as={'b'}>Unicorn Hunters</Text>
             <Text fontSize={'lg'} pb={10}>TV Show</Text>
-            <VideoComponent2 />
+   
             <Button>View Project</Button>
           </Box>
         </Flex>
