999f4ac0d7afa0ee149f55565ac2de6bf42acb30
Tomás Maritano <tomymaritano@gmail.com>
2024-07-23 00:44:46 -0300

    updates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './layouts/Layout';
-import HomePage from './pages/HomePage';
-import ProjectsPage from './pages/ProjectsPage';
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from './theme';
+import HomePage from './pages/HomePage';
+import ProjectsPage from './pages/ProjectsPage';
 
 const App = () => {
     return (
