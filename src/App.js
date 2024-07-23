b3df5b039e5ff5346685459751b7a7f058e4e7b6
Tomás Maritano <tomymaritano@gmail.com>
2024-07-23 00:51:26 -0300

    updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './layouts/Layout';
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from './theme';
@@ -12,7 +12,8 @@ const App = () => {
             <Layout>
                 <Router>
                     <Routes>
-                        <Route path="/" element={<HomePage />} />
+                        <Route path="/" element={<Navigate replace to="/bio" />} />
+                        <Route path="/bio" element={<HomePage />} />
                         <Route path="/work" element={<ProjectsPage />} />
                     </Routes>
 
